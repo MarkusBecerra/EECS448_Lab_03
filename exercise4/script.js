f8d6e48d24450a85e93e247c87ea7204312e2cb0
Markus Becerra <markusbecerra@ku.edu>
2019-10-01 22:27:51 -0500

    Created Exercise 5 and edited Exercise 4

--- exercise4/script.js
+++ exercise4/script.js
@@ -1,18 +1,18 @@
 
 function update()
 {
-let redBorder = document.getElementById("red").value;
-let greenBorder = document.getElementById("green").value;
-let blueBorder = document.getElementById("blue").value;
+let red = document.getElementById("red").value;
+let green = document.getElementById("green").value;
+let blue = document.getElementById("blue").value;
 let borderWidth = document.getElementById("width").value;
 let redBackground = document.getElementById("redBackground").value;
 let greenBackground = document.getElementById("greenBackground").value;
 let blueBackground = document.getElementById("blueBackground").value;
 
 
-document.getElementById("sampletext").style.borderColor = "rgb(" + redBorder + "," + greenBorder + "," + blueBorder + ")";
+document.getElementById("sampletext").style.borderColor = "rgb(" + red + "," + green + "," + blue + ")";
 document.getElementById("sampletext").style.backgroundColor = "rgb("+redBackground+","+greenBackground+","+blueBackground+")";
-document.getElementById("sampletext").style.borderWidth = borderWidth;
+document.getElementById("sampletext").style.width = borderWidth;
 alert("Current Border Color:" + sampletext.style.borderColor + " Current Border Width: " + sampletext.style.borderWidth + "Current Background Color: " + sampletext.style.backgroundColor);
 
 }
