5b846ee1f36222f2d84310dacf9b1c23937f48c8
Markus Becerra <markusbecerra@ku.edu>
2019-10-01 21:55:01 -0500

    attempt at fixing exercise4?

--- exercise4/script.js
+++ exercise4/script.js
@@ -1,7 +1,6 @@
 
 function update()
 {
-let sampletext = document.getElementById("sampletext");
 let redBorder = document.getElementById("red").value;
 let greenBorder = document.getElementById("green").value;
 let blueBorder = document.getElementById("blue").value;
@@ -11,9 +10,9 @@ let greenBackground = document.getElementById("greenBackground").value;
 let blueBackground = document.getElementById("blueBackground").value;
 
 
-sampletext.style.borderColor = "rgb(" + redBorder + "," + greenBorder + "," + blueBorder + ")";
-sampletext.style.backgroundColor = "rgb("+redBackground+","+greenBackground+","+blueBackground+")";
-sampletext.style.borderWidth = borderWidth;
+document.getElementById("sampletext").style.borderColor = "rgb(" + redBorder + "," + greenBorder + "," + blueBorder + ")";
+document.getElementById("sampletext").style.backgroundColor = "rgb("+redBackground+","+greenBackground+","+blueBackground+")";
+document.getElementById("sampletext").style.borderWidth = borderWidth;
 alert("Current Border Color:" + sampletext.style.borderColor + " Current Border Width: " + sampletext.style.borderWidth + "Current Background Color: " + sampletext.style.backgroundColor);
 
 }
